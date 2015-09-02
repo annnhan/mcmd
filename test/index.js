a144b01afb70f1de4d7d3e7747fe4b5d24d1f82a
an.han <an.han@qunar.com>
2015-09-02 22:26:56 +0800

    test

--- test/index.js
+++ test/index.js
@@ -2,12 +2,9 @@
  * @file
  * Created by an.han on 15/9/2.
  */
-define(function (require, exports, module) {
+define(function (require) {
     var log = require('log.js');
     var msg = require('msg.js');
 
-    exports.log = function () {
-        log(msg);
-    }
-
+    log(msg);
 });
